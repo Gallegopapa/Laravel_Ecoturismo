c1278d709bd8f722464d14498d0cf1691dc98d5f
Juan Jose Lopez Pineda <juanjolopin@gmail.com>
2026-02-05 11:13:46 -0500

    palabras groseras

--- config/profanity.php
+++ config/profanity.php
@@ -12,13 +12,13 @@ return [
         'cabron', 'cabrón', 'cabrons', 'cabróna', 'cabrona',
         'gilipollas', 'gilipolla', 'gilipollez',
         'pendejo', 'pendeja', 'pendejos', 'pendejas',
-        'coño', 'coños', 'coñazo', 'coñ*', 'cojones', 'cojon', 'cojones',
+        'coño', 'coños', 'coñazo', 'coñ*', 'cojones', 'cojon', 'cojones', 'marica', 'marika', 'mk', 'guevon', 'huevon', 'malparido', 'malparida', 'malparidos', 'malparidas', 'pirobo', 'piroba', 'pirobas', 'pirobos', 
         'zorra', 'zorro', 'zorras', 'zorros', 'zorr*',
         'idiota', 'idiotas', 'imbecil', 'imbécil', 'imbeciles', 'tonto', 'tontos', 'tarado', 'tarados',
         'puta\s+madre',
 
         // Otros insultos comunes
-        'perra', 'perro', 'culero', 'culera', 'culero', 'golfa', 'putón', 'puton', 'maricon', 'maricón',
+        'perra', 'culero', 'culera', 'culero', 'golfa', 'putón', 'puton', 'maricon', 'maricón', 'maricona', 'maricones', 'mariconas',
         'chinga', 'chingada', 'chingar', 'chingao', 'chingado', 'chingados',
         'sidoso', 'sidosa'
     ],
