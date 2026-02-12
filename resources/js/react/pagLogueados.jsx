3def72bedd593ec247a04696c71523cbd5f18903
Jeycob-ui <goldarmony8776@gmail.com>
2026-02-12 08:30:55 -0500

    pagcentral

--- resources/js/react/pagLogueados.jsx
+++ resources/js/react/pagLogueados.jsx
@@ -4,9 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import Header2 from "@/react/components/Header2/Header2";
 import Footer from "@/react/components/Footer/Footer";
-// import Card from '@/react/components/Cards/Card';
 import Slider from "@/react/components/slider/Slider";
-import HomePage from "./components/HomePage";
 
 function PagLogueados() {
   const navigate = useNavigate();
@@ -42,7 +40,7 @@ function PagLogueados() {
     <div className="page-layout">
       <Header2 />
       <main className="page-content">
-        <HomePage loggedIn={true} user={user} />
+        <Slider />
       </main>
       <Footer />
     </div>
