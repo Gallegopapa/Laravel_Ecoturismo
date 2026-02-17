9c5cff8e41646d61986507433a5edccedc24717c
simon23051997@gmail.com <simon23051997@gmail.com>
2026-02-17 11:57:52 -0500

    ....

--- resources/js/react/admin/CompanyDashboard.jsx
+++ resources/js/react/admin/CompanyDashboard.jsx
@@ -179,6 +179,7 @@ const CompanyDashboard = () => {
     useEffect(() => {
         if (selectedPlaceId) {
             loadSchedules(selectedPlaceId);
+            loadPlaceDetails(selectedPlaceId);
         }
     }, [selectedPlaceId]);
 
